8f5029041c7797773405c304fccba517ae9a9ee0
XJM <978966364@qq.com>
2022-02-26 17:24:27 +0800

    test2

---
+++ javaweb-common/javaweb-common-redis/src/main/java/com/javaweb/common/redis/config/test2.java
@@ -0,0 +1,11 @@
+package com.javaweb.common.redis.config;
+
+/**
+ * @author XJM
+ * @title: test2
+ * @projectName fortool_test
+ * @description: TODO
+ * @date 2022/2/2617:24
+ */
+public class test2 {
+}
