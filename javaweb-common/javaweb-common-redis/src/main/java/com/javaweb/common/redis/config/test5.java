aedf8878d0f0df89a21a0a26466e2cf82d9093b9
XJM <978966364@qq.com>
2022-02-26 17:27:22 +0800

    test5

---
+++ javaweb-common/javaweb-common-redis/src/main/java/com/javaweb/common/redis/config/test5.java
@@ -0,0 +1,11 @@
+package com.javaweb.common.redis.config;
+
+/**
+ * @author XJM
+ * @title: test5
+ * @projectName fortool_test
+ * @description: TODO
+ * @date 2022/2/2617:27
+ */
+public class test5 {
+}
